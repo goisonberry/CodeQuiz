1219672802074641631a96722e8d00e1ed020356
goisonberry <gerson.marroquin@gmail.com>
2020-06-02 16:02:49 -0700

    added a script js and outlined what needs to be added in order to get the quiz working.

---
+++ assets/script.js
@@ -0,0 +1,10 @@
+// Things that need to be added
+
+// variables for index, countdown and score
+// start the quiz when "start" button is clicked in the html file
+// have the questions populate
+// have the choices populate that relate to the question
+// a way to replace previous question, with new question without needing to create a new html for each question
+// see if the use selected the right answer
+// timer needs to be set
+// store initial and high scores without deleting them automatically.
