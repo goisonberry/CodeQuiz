0de399df3e6be5c49ce85f3ea0ead15c8ed68663
goisonberry <gerson.marroquin@gmail.com>
2020-06-01 18:55:20 -0700

    created a questions variable and added the 5 questions.

--- assets/questions.js
+++ assets/questions.js
@@ -1,1 +1,33 @@
-var questions = [];
+var questions = [
+  {
+    title: "What tag tells the brower the code is written in HTML?",
+    choices: ["<!DOCtypeHTML>", "<title>", "<html>", "<header>", "<h1>"],
+    answer: "<html>",
+  },
+  {
+    title: "How would you comment in Javescript?",
+    choices: ["<!-- -->", "/*====*/", "$()", "//", "None of the above"],
+    answer: "//",
+  },
+  {
+    title: "In Javascript, which of the following accepts a boolean?",
+    choices: ["confirm", "prompt", "alerts", "document", "class"],
+    answer: "confirm",
+  },
+  {
+    title: "What is the purpose of console.log?",
+    choices: [
+      "tell you stuff",
+      "close a function",
+      "send you and email",
+      "debug",
+      "store information",
+    ],
+    answer: "debug",
+  },
+  {
+    title: "What file type does JQuery assist you with?",
+    choices: ["html", "css", "pdf", "javascript", "All of the above"],
+    answer: "javascript",
+  },
+];
